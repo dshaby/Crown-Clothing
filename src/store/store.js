fbdcc2e68a85ad452acb733864a17033603819bf
dshaby <daniel@businessesoftheearth.org>
2022-06-22 11:46:14 -0700

    Update store.js - adding our own loggerMiddleware
    helps us understand redux rerenders better.

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,23 @@
 // import { configureStore } from "@reduxjs/toolkit";
 import { compose, createStore, applyMiddleware } from "redux";
-import logger from "redux-logger";
+// import logger from "redux-logger";
 import { rootReducer } from "./root-reducer";
 
-const middleWares = [logger];
+const loggerMiddleware = (store) => (next) => (action) => {
+  if (!action.type) {
+    return next(action);
+  }
+
+  console.log("Type: ", action.type);
+  console.log("Payload: ", action.payload);
+  console.log("Current (i.e. previous) state: ", store.getState());
+
+  next(action); // here, middleware passes action from store to mini-reducers
+
+  console.log("Next/Final State: ", store.getState());
+};
+
+const middleWares = [loggerMiddleware];
 
 const composedEnhancers = compose(applyMiddleware(...middleWares));
 
