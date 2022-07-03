7b43ac89d6d005374b837294aad8ff74b75fb0d6
dshaby <88353394+dshaby@users.noreply.github.com>
2022-07-03 16:59:03 -0700

    Update root-reducer.js

--- src/store/root-reducer.js
+++ src/store/root-reducer.js
@@ -1,5 +1,4 @@
 import { combineReducers } from "redux";
-import { cartReducer } from "./cart/cart.reducer";
 import { categoriesReducer } from "./categories/categories.reducer";
 import { userReducer } from "./user/user.reducer";
 import { cartReducer } from "./cart/cart.reducer";
