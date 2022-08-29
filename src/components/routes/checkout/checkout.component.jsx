d7d619ff8f080d679b4e232d7c232ef609aa03dc
dshaby <88353394+dshaby@users.noreply.github.com>
2022-08-28 19:31:57 -0700

    create checkout component??

---
+++ src/components/routes/checkout/checkout.component.jsx
@@ -0,0 +1,40 @@
+// import { useContext } from "react";
+import { useSelector } from "react-redux";
+// import { CartContext } from "../../../context/cart.context";
+import {
+  selectCartItems,
+  selectCartTotal,
+} from "../../../store/cart/cart.selector";
+import CheckoutHeader from "../../checkout-header/header-block.component";
+import CheckoutItem from "../../checkout-item/checkout-item.component";
+import PaymentForm from "../../payment-form/payment-form.component";
+import "./checkout.styles.scss";
+
+const CheckOut = () => {
+  // const { cartItems, cartTotal } = useContext(CartContext);
+  const cartItems = useSelector(selectCartItems);
+  const cartTotal = useSelector(selectCartTotal);
+
+  return (
+    <>
+      <div className="checkout-container">
+        <div className="checkout-header">
+          <CheckoutHeader header="Product" />
+          <CheckoutHeader header="Description" />
+          <CheckoutHeader header="Quantity" />
+          <CheckoutHeader header="Price" />
+          <CheckoutHeader header="Remove" />
+        </div>
+        {cartItems.map((cartItem) => (
+          <CheckoutItem key={cartItem.id} cartItem={cartItem} />
+        ))}
+        <div className="total">
+          <span className="total">TOTAL: ${cartTotal} </span>
+        </div>
+        <PaymentForm />
+      </div>
+    </>
+  );
+};
+
+export default CheckOut;
