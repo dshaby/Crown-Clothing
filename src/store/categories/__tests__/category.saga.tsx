c3c3e3b304a78e546a997dd6bb149132e0980f73
Daniel Shaby <shaby@Daniels-MacBook-Air.local>
2024-04-18 21:44:40 -0700

    add categories saga tests

---
+++ src/store/categories/__tests__/category.saga.tsx
@@ -0,0 +1,53 @@
+import {expectSaga, testSaga} from 'redux-saga-test-plan';
+import { categoriesSaga, fetchCategoriesAsync, onFetchCategories } from "../categories.saga"
+import { call } from 'redux-saga/effects';
+import { CATEGORIES_ACTION_TYPES, CategoryItem } from '../categories.types';
+import { fetchCategoriesFailed, fetchCategoriesStart, fetchCategoriesSuccess } from '../categories.action';
+import { getCategoriesAndDocuments } from '../../../utils/firebase/firebase.utils';
+import { mockCategoriesData } from '../mocks';
+import { throwError } from 'redux-saga-test-plan/providers';
+
+describe('category sagas', () => {
+    test('categoriesSaga', () => {
+        testSaga(categoriesSaga)
+        .next()
+        .all([call(onFetchCategories)])
+        .next()
+        .isDone();
+    });
+
+    test('onFetchCategories', () => {
+        testSaga(onFetchCategories)
+        .next()
+        .takeLatest(
+            CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START,
+            fetchCategoriesAsync
+        )
+        .next()
+        .isDone();
+    });
+
+    test('fetchCategoriesAsync success', () => {
+
+        const mockCategoriesArray: CategoryItem[] = [
+            {id: 1, name: 'Product 1', imageUrl: '', price: 1},
+            {id: 2, name: 'Product 2', imageUrl: '', price: 2}
+        ]
+
+        expectSaga(fetchCategoriesAsync)
+            .provide([[call(getCategoriesAndDocuments), mockCategoriesArray]])
+            .put(fetchCategoriesSuccess(mockCategoriesData))
+            .run();
+    });
+
+    test('fetchCategoriesAsync failure', () => {
+        const mockErr = new Error('An error occurred');
+
+        expectSaga(fetchCategoriesAsync)
+            .provide([
+                [call(getCategoriesAndDocuments), throwError(mockErr)]
+            ])
+            .put(fetchCategoriesFailed(mockErr))
+            .run();
+    })
+});
